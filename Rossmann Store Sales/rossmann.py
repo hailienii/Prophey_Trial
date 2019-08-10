7182a8148768a56bb5e58d8417a5f2028d36e061
XiangkeChen <38060458+XiangkeChen@users.noreply.github.com>
2019-08-10 12:17:08 -0500

    Add files via upload

--- Rossmann Store Sales/rossmann.py
+++ Rossmann Store Sales/rossmann.py
@@ -1,64 +1,45 @@
-import warnings
-warnings.filterwarnings("ignore")
-
-# loading packages
-# basic + dates 
-import numpy as np
 import pandas as pd
-from pandas import datetime
-
-# data visualization
-import matplotlib.pyplot as plt
-import seaborn as sns # advanced vizs
-# %matplotlib inline
-
-# statistics
-from statsmodels.distributions.empirical_distribution import ECDF
-
-# time series analysis
-from statsmodels.tsa.seasonal import seasonal_decompose
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-
-# prophet by Facebook
 from fbprophet import Prophet
 
-
 # importing data
-df = pd.read_csv("train.csv",  low_memory = False)
+df = pd.read_csv("train.csv",  low_memory=False)
+test_df = pd.read_csv("test.csv", low_memory=False)
 
 # remove closed stores and those with no sales
 df = df[(df["Open"] != 0) & (df['Sales'] != 0)]
 
-# sales for the store number 1 (StoreType C)
+# sales for store number 1
 sales = df[df.Store == 1].loc[:, ['Date', 'Sales']]
 
-# reverse to the order: from 2013 to 2015
-sales = sales.sort_index(ascending = False)
+# reverse the order: from 2013 to 2015
+sales = sales.sort_index(ascending=False)
 
-# to datetime64
+# to datetime
 sales['Date'] = pd.DatetimeIndex(sales['Date'])
-# sales.dtypes
-
 
 # from the prophet documentation every variables should have specific names
-sales = sales.rename(columns = {'Date': 'ds','Sales': 'y'})
+sales = sales.rename(columns={'Date': 'ds', 'Sales': 'y'})
 
-# create holidays dataframe
-state_dates = df[(df.StateHoliday == 'a') | (df.StateHoliday == 'b') & (df.StateHoliday == 'c')].loc[:, 'Date'].values
+# create dataframe of holidays in the dataset
+state_dates = df[(df.StateHoliday == 'a') | (df.StateHoliday == 'b')
+                 & (df.StateHoliday == 'c')].loc[:, 'Date'].values
 school_dates = df[df.SchoolHoliday == 1].loc[:, 'Date'].values
 
-state = pd.DataFrame({'holiday': 'state_holiday','ds': pd.to_datetime(state_dates)})
-school = pd.DataFrame({'holiday': 'school_holiday','ds': pd.to_datetime(school_dates)})
+state = pd.DataFrame({'holiday': 'state_holiday',
+                      'ds': pd.to_datetime(state_dates)})
+school = pd.DataFrame({'holiday': 'school_holiday',
+                       'ds': pd.to_datetime(school_dates)})
 
-holidays = pd.concat((state, school))      
+holidays = pd.concat((state, school))
 holidays.head()
 
 # set the uncertainty interval to 95% (the Prophet default is 80%)
-my_model = Prophet(interval_width = 0.95, holidays = holidays, daily_seasonality= True)
+my_model = Prophet(interval_width=0.95, holidays=holidays,
+                   daily_seasonality=True)
 my_model.fit(sales)
 
-# dataframe that extends into future 6 weeks 
-future_dates = my_model.make_future_dataframe(periods = 6*7)
+# dataframe that extends into future 6 weeks
+future_dates = my_model.make_future_dataframe(periods=6*7)
 
 
 # predictions
@@ -67,10 +48,7 @@ forecast = my_model.predict(future_dates)
 # preditions for last week
 forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail(7)
 
-# The forecast object here is a new dataframe that includes a column yhat with the forecast, as well as columns for components and uncertainty intervals.
-fc = forecast[['ds', 'yhat']].rename(columns = {'Date': 'ds', 'Forecast': 'yhat'})
-
 # visualizing predicions
-my_model.plot(forecast);
+my_model.plot(forecast)
+my_model.plot_components(forecast)
 
-my_model.plot_components(forecast);
